c7143a29805f05aedd4f67a842664159f7db84cd
shivang.m <Shiv@ngM@22>
2026-06-09 11:56:19 +0530

    fix: separate app and library namespaces

---
+++ app/src/main/java/com/vrinsoft/filepickerdemo/SampleActivity.kt
@@ -0,0 +1,20 @@
+package com.vrinsoft.filepickerdemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sample)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
